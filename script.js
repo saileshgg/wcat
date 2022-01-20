ee223e003a7ccd20543ea750aeea999a586f02c1
saileshgg <saileshgurung36@gmail.com>
2022-01-20 05:59:45 +0530

    process argv

--- script.js
+++ script.js
@@ -2,8 +2,15 @@
 
 const fs = require("fs");
 
-let data = fs.readFileSync("C:\\Users\\saile\\Documents\\GitHub\\wcat\\abc.txt","utf-8");
+//let data = fs.readFileSync("C:\\Users\\saile\\Documents\\GitHub\\wcat\\abc.txt","utf-8");
 
-fs.writeFileSync("def.txt","What is up nigga");
+//fs.writeFileSync("def.txt","What is up nigga");
 
-console.log(data);
+//console.log(data);
+
+//process.argv-------------------------------------------------
+
+let filename = process.argv[2];
+
+let ans = fs.readFileSync(filename,"utf-8");
+console.log(ans);
