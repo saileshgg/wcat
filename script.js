e845748f26e2d3c75fa9b14d6e9a9636dddf8d2c
saileshgg <saileshgurung36@gmail.com>
2022-01-20 06:52:14 +0530

    append and write command

--- script.js
+++ script.js
@@ -10,10 +10,21 @@ const fs = require("fs");
 
 //process.argv-------------------------------------------------
 
+let processData = process.argv;
 let filename;
 
-for (var i = 2; i < process.argv.length; i++) {
-    filename = process.argv[i];
-    let ans = fs.readFileSync(filename, "utf-8");
-    console.log(ans);
+if(processData[2] === "w") {
+    fs.writeFileSync(processData[3],processData[4]);
+    let ans = fs.readFileSync("abc.txt", "utf-8");
+    console.log("This is file after write operation" + "\n" + ans);
+} else if(processData[2] === "a") {
+    let previousData = fs.readFileSync(processData[3],"utf-8");
+    fs.writeFileSync(processData[3],previousData + "\n" + processData[4]);
+} else {
+    for (var i = 2; i < processData.length; i++) {
+        filename = process.argv[i];
+        let ans = fs.readFileSync(filename, "utf-8");
+         console.log(ans);
+    }
 }
+
